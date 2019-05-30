acd3e7528ea3c1b24cdc4476a9ff8ab17bcffe70
Hannes Eichblatt <git@hanneseichblatt.de>
2019-05-30 20:06:56 +0200

    Update build.sh, add GitLab CI config

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+
 set +e
-OLDPWD="$PWD"
+
+log() {
+        echo -e "$0: $*"
+}
 
 for df in $(find . -name Dockerfile) ; do
         image=$(dirname "$df"|cut -d\/ -f2-)
-        echo Building "$image"..
-        cd "$image"
+        log Building "$image"..
+        pushd "$image"
         docker build --rm --no-cache --pull=true -t local/"$image" . || ERRORS="$ERRORS $df"
-        cd "$OLDPWD"
+        popd
 done
 
 [ -z "${ERRORS}" ] || ( echo ERRORS\: "$ERRORS" ; exit 1 )
